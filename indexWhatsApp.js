fdee966a662c60c64e93e30507a1c4b501343242
Olef <olegbestboy@mail.ru>
2018-11-08 15:51:30 +0300

    test

--- indexWhatsApp.js
+++ indexWhatsApp.js
@@ -9,8 +9,9 @@ app.use(bodyParser.json());
 
 
 app.post("/test", function (req, res) {
-    console.log("Rabotaet")
-    console.log(req.body)
+    console.log("Rabotaet");
+    console.log(req.body);
+    res.send(200)
 })
 
 
