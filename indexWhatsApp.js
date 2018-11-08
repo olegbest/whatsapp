5d7eaa6bf72ec50f8acc5f179a31bed9f252120f
Olef <olegbestboy@mail.ru>
2018-11-08 16:13:39 +0300

    test

--- indexWhatsApp.js
+++ indexWhatsApp.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+const curl = new (require('curl-request'))();
+const twilio = require('twilio');
+const accountSid = 'AC4f2059c0fb4f7e820c7dd015427c8b56'; // Your Account SID from www.twilio.com/console
+const authToken = 'c8d88b4d474a866bf8b90abd5a5752dc';
+const client = new twilio(accountSid, authToken);
 
 const app = express();
 const port = 5125;
@@ -11,10 +17,16 @@ app.use(bodyParser.json());
 app.post("/test", function (req, res) {
     console.log("Rabotaet");
     console.log(req.body);
-    res.send(200)
-})
+    client.messages.create({
+        body: 'Hello from Node',
+        to: `${req.body.from}`,  // Text this number
+        from: '+14155238886' // From a valid Twilio number
+    })
+        .then((message) => console.log(message.sid));
+    res.sendStatus(200)
+});
 
 
 app.listen(port, function () {
     console.log("running: port")
-})
+});
