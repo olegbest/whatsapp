4b3e760c155e5bb6293f25855555643123007a00
Olef <olegbestboy@mail.ru>
2018-11-08 15:48:23 +0300

    test

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5125;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.post("/test", function (req, res) {
+    console.log("Rabotaet")
+    console.log(req.body)
+})
+
+
+app.listen(port, function () {
+    console.log("running: port")
+})
